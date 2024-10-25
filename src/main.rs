1e6a88971846a91676d5ee3f6a141f6ca7edf781
eduardo.delinski <eduardo_delinski@hotmail.com>
2024-10-25 00:46:49 -0300

    alteracoes para funcionar com a rusty_engine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{ops::{Deref, DerefMut}, time::Duration};
+
+use rusty_engine::prelude::*;
+
+#[derive(Resource)]
+struct GameState {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = Game::new();
+
+    game.add_logic(game_logic);
+
+    game.run(GameState {
+    });
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
 }
